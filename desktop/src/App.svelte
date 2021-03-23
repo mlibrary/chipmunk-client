d70a24f703fd73368bdab23ba42a314db1266731
Noah Botimer <botimer@umich.edu>
2021-03-23 10:13:37 -0400

    Trim BDD skeleton to bare minimum

--- desktop/src/App.svelte
+++ desktop/src/App.svelte
@@ -1,9 +1,7 @@
 <script>
 </script>
 
-<div class="viewport-container">
-  <h1 id="test-text">This is NOT a test.</h1>
-</div>
+<h1 id="test-text">This is NOT a test.</h1>
 
 <svelte:head>
 <script type="module" src="https://unpkg.com/@umich-lib/components@v1/dist/umich-lib/umich-lib.esm.js"></script>
